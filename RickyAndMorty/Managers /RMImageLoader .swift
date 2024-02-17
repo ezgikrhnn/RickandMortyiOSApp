ceda7dc258163fc4c1f646e929f802b9734991bd
Ezgi Karahan <karahanezgi64@gmail.com>
2024-02-17 13:36:40 +0300

    More updates

---
+++ RickyAndMorty/Managers /RMImageLoader .swift
@@ -0,0 +1,15 @@
+//
+//  ImageManager .swift
+//  RickyAndMorty
+//
+//  Created by Ezgi Karahan on 16.02.2024.
+//
+
+import Foundation
+
+final class ImageLoader{
+    
+    static let shared = ImageLoader()
+    
+}
+
---
+++ RickyAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickyAndMorty
+//
+//  Created by Ezgi Karahan on 11.02.2024.
+//
+
+import UIKit
+
+class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
